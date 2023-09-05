bc8f4bad12b56d47574c62c6945d48406f184fd8
TempusFugit <tempus9fugit@gmail.com>
2023-09-05 22:31:46 +0100

    BookingTable model

--- capstone_project/LittleLemon/restaurant/models.py
+++ capstone_project/LittleLemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+class BookingTable(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateField(db_index=True)
+    
